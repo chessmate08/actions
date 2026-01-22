12d91c6a69a17a52194f5fc830cb0016745446ed
chessmate08 <jonahbernard96@gmail.com>
2026-01-22 00:27:19 -0700

    npm_test

---
+++ node/index.test.js
@@ -0,0 +1,14 @@
+const app = require('./index')
+const request = require('supertest')
+
+describe('GET /', () => {
+  it('\'/\' should display "Hello World" ', async () => {
+    const response = await request(app).get('/');
+
+    // Check if the response status is 200 (OK)
+    expect(response.statusCode).toBe(200);
+
+    // Check if the response body (HTML string) contains the exact text
+    expect(response.text).toContain('Hello World!');
+  });
+});
